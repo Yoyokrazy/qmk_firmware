c92d3c9fe5e286ee602853a354ac9db25d8ba411
Michael Lively <mdlively@wisc.edu>
2020-08-16 16:00:20 -0700

    we did it -- shit finally works
    macros are done and layers work with knobs

--- keyboards/dmqdesign/spin/keymaps/ml_macro/keymap.c
+++ keyboards/dmqdesign/spin/keymaps/ml_macro/keymap.c
@@ -1,4 +1,8 @@
 #include QMK_KEYBOARD_H
+#define _MEDIA 0
+#define _RGB 1
+
+uint8_t currentLayer;
 
 enum custom_keycodes {
     DTP_LFT = SAFE_RANGE,
@@ -11,19 +15,19 @@ enum custom_keycodes {
 //----------------------------------------------------------------------------------------------------------------------------------------
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( /* Base */
-                _______,      _______,      RGB_TOG,        TG(1),
+    [_MEDIA] = LAYOUT( /* Base */
+                _______,      _______,      RGB_TOG,        TO(_RGB),
                 KC_MPRV,      KC_MPLY,      KC_MNXT,        _______,
                 _______,      KC_PSCR,      _______,        _______,
                 DTP_LFT,      DTP_MID,      DTP_RGT
                 ),
 
-    [1] = LAYOUT( /* LAYER 1 -- TAP UPPER KNOB */
-                _______,      _______,      RGB_MOD,        _______,
+    [_RGB] = LAYOUT( /* LAYER 1 -- TAP UPPER KNOB */
+                _______,      _______,      RGB_MOD,        TO(_MEDIA),
                 _______,      _______,      _______,        _______,
                 _______,      _______,      _______,        _______,
-                _______,      _______,      _______
-                ),
+                _______,      KC_NO,        _______
+              ),
 };
 
 //----------------------------------------------------------------------------------------------------------------------------------------
@@ -64,19 +68,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 0) { /* First encoder */
-    switch (currentLayer) {
+    switch(biton32(layer_state)) {
       case 0: // BASE LAYER
         if (clockwise) {
           rgblight_increase_hue(); //Cycle through the RGB hue
         } else {
           rgblight_decrease_hue();
         }
+        break;
+
       case 1: // FIRST LAYER
         if (clockwise) {
           rgblight_increase_val(); //Change brightness on the RGB LEDs
         } else {
           rgblight_decrease_val();
         }
+        break;
+
     }
   }  else if (index == 1) { /* second encoder -- EMPTY */
 
