09ad6a6ee18ade469dd97963fd59296b11a4d778
Michael Lively <mdlively@wisc.edu>
2020-09-01 12:25:28 -0500

    added tidbit code

---
+++ keyboards/tidbit/keymaps/via/keymap.c
@@ -0,0 +1,71 @@
+#include QMK_KEYBOARD_H
+#include "remote_kb.h"
+#include "bitc_led.h"
+
+#define _BASE     0
+#define _VIA1     1
+#define _VIA2     2
+#define _VIA3     3
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT(
+     KC_TRNS,  KC_TRNS, KC_TRNS, \
+  KC_7, KC_8,     KC_9, KC_PSLS, \
+  KC_4, KC_5,     KC_6, KC_PAST, \
+  KC_1, KC_2,     KC_3, KC_PMNS, \
+  KC_0, KC_DOT, KC_ENT, KC_PPLS  \
+  ),
+
+  [_VIA1] = LAYOUT(
+           KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
+  ),
+
+  [_VIA2] = LAYOUT(
+           KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
+  ),
+
+  [_VIA3] = LAYOUT(
+           KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
+  ),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  process_record_remote_kb(keycode, record);
+  return true;
+}
+
+void matrix_init_user(void) {
+  matrix_init_remote_kb();
+  set_bitc_LED(LED_OFF);
+}
+
+void matrix_scan_user(void) {
+  matrix_scan_remote_kb();
+}
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+  if (clockwise) {
+    tap_code(KC_VOLU);
+  } else {
+    tap_code(KC_VOLD);
+  }  
+}
+
+void led_set_kb(uint8_t usb_led) {
+  if (usb_led & (1<<USB_LED_NUM_LOCK))
+    set_bitc_LED(LED_DIM);
+  else
+    set_bitc_LED(LED_OFF);
+}
